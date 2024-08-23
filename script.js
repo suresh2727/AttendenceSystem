7b5996cf35cb31c6769f83d6620fd23a71e22551
sureshmistry <coolsuru2727@gmail.com>
2024-08-23 21:33:34 +0530

    Inserted More data

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 var btn = document.getElementById("btn-location");
-var heading = document.createElement('h1');
-var body = document.body;
+
 function gotLocation(position) {
-    let leti = position.coords.latitude;
-    let lon = position.coords.longitude;
-  heading.innerHTML=`Latitude : ${leti}  and Longitude : ${lon} `;
-  body.appendChild(heading);
+  let leti = position.coords.latitude;
+  let lon = position.coords.longitude;
 }
 
 function failedToGet() {
-  console.log("There was some issue");
+  alert("Allow Location");
 }
-
-btn.addEventListener("click", () => {
+setTimeout(() => {
   navigator.geolocation.getCurrentPosition(gotLocation, failedToGet);
-  
-});
+}, 500);
 
